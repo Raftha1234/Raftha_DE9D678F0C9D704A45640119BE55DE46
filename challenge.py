649bc5545ef6f889cdbb147bf1df0b1085d9f6e7
Raftha1234 <144211695+Raftha1234@users.noreply.github.com>
2023-10-22 20:00:46 +0530

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -11,3 +11,10 @@ if isleepyear(year):
   print(year,"is a leep year")
 else:
   print(year,"is not a leep year")
+
+year = int(input("Enter a year: "))
+if (year % 400 == 0) and (year % 100 == 0):
+  print("{0} is a leap year".format(year))
+elif (year % 4 == 0) and (year % 100 != 0):
+  print("{0} is a leap year".format(year)) else:   print("{0} is not a leap year".format(year))
+  
